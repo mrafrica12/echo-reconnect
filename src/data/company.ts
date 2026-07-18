b50e5220ddc1f54ca34959d6a7d88d6245665bdd
Ben Ngie <katheka@gmail.com>
2026-07-18 11:32:01 -0400

    Delete src directory

--- src/data/company.ts
+++
@@ -1,8 +0,0 @@
-export const COMPANY = {
-  name: "Echo Reconnect",
-  location: "Atlanta, GA",
-  phoneDisplay: "770-230-5060",
-  phoneHref: "tel:+17702305060",
-  email: "info@echoreconnect.com",
-  emailHref: "mailto:info@echoreconnect.com",
-} as const;
