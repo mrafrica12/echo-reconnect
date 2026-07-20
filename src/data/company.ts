fff66b0b41f583351a89f735c0395f37592d3f75
Ben Ngie <katheka@gmail.com>
2026-07-20 19:55:16 -0400

    Add files via upload

---
+++ src/data/company.ts
@@ -0,0 +1,8 @@
+export const COMPANY = {
+  name: "Echo Reconnect",
+  location: "Atlanta, GA",
+  phoneDisplay: "+1 478-834-9871",
+  phoneHref: "tel:+14788349871",
+  email: "info@echoreconnect.com",
+  emailHref: "mailto:info@echoreconnect.com",
+} as const;
